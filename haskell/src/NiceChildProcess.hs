7b24b6e8216c1267f20c67445f97b99cc94ada82
Callum Rogers <callumrogers@hotmail.co.uk>
2015-07-18 19:11:10 +0100

    Works reading one line

--- haskell/src/NiceChildProcess.hs
+++ haskell/src/NiceChildProcess.hs
@@ -1,13 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module NiceChildProcess (
-  CP.spawn
-, readLine
-) where
+module NiceChildProcess where
 
+import Control.Concurrent.MVar
+import Control.Applicative
+import Data.IORef
+import Data.Monoid
+import Data.Text (Text)
+import qualified Data.Text as T
 import GHCJS.Types
+import GHCJS.Foreign
 
 import qualified Node.ChildProcess as CP
 
-readLine :: CP.ChildProcess -> IO JSString
-readLine childProcess = return ""
+type Command = Text
+type Arg = Text
+type Directory = Text
+
+newtype ChildProcess = ChildProcess CP.ChildProcess
+
+spawn :: Command -> [Arg] -> Directory -> IO ChildProcess
+spawn command args cwd = ChildProcess <$> CP.spawn (toJSString command) (map toJSString args) (toJSString cwd)
+
+readLine :: ChildProcess -> IO Text
+readLine (ChildProcess childProcess) = do
+  outStream <- CP.stdout childProcess
+  sema <- newEmptyMVar
+  CP.onData outStream $ \buffer -> do
+    str <- CP.toString buffer
+    putMVar sema (T.dropWhileEnd (== '\n') $ fromJSString str)
+  takeMVar sema
--- haskell/test/NiceChildProcessSpec.hs
+++ haskell/test/NiceChildProcessSpec.hs
@@ -7,6 +7,7 @@ import GHCJS.Types
 import NiceChildProcess
 
 should = it
+xshould _ _ = return ()
 
 spec :: Spec
 spec = do
@@ -15,3 +16,10 @@ spec = do
       childProcess <- spawn "echo" ["lol"] "."
       line <- readLine childProcess
       line `shouldBe` "lol"
+
+    xshould "read two lines from a process" $ do
+      childProcess <- spawn "cat" ["test-data/multiline-file"] "."
+      line1 <- readLine childProcess
+      line2 <- readLine childProcess
+      line1 `shouldBe` "line one"
+      line2 `shouldBe` "line two"
