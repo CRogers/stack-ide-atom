476b2df1863895e83106d680d923fe19a5583afe
Callum Rogers <callumrogers@hotmail.co.uk>
2015-07-18 20:22:23 +0100

    Failed attempt to use TChan

--- haskell/src/NiceChildProcess.hs
+++ haskell/src/NiceChildProcess.hs
@@ -2,13 +2,14 @@
 
 module NiceChildProcess where
 
-import Control.Concurrent.MVar
 import Control.Applicative
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TChan
 import Control.Monad
-import Data.IORef
 import Data.Monoid
 import Data.Text (Text)
 import qualified Data.Text as T
+import Data.Traversable
 import GHCJS.Types
 import GHCJS.Foreign
 
@@ -18,30 +19,27 @@ type Command = Text
 type Arg = Text
 type Directory = Text
 
-type LineBuffer = IORef [Text]
-type Semaphore = MVar ()
+type Line = Text
+type LineQueue = STM (TChan Line)
 
-data ChildProcess = ChildProcess CP.ChildProcess LineBuffer Semaphore
+data ChildProcess = ChildProcess CP.ChildProcess LineQueue
 
 spawn :: Command -> [Arg] -> Directory -> IO ChildProcess
 spawn command args cwd = do
   childProcess <- CP.spawn (toJSString command) (map toJSString args) (toJSString cwd)
   outStream <- CP.stdout childProcess
-  lineBuffer <- newIORef []
-  sema <- newEmptyMVar
+  let lineQueue = newTChan
   CP.onData outStream $ \buffer -> do
     text <- fromJSString <$> CP.toString buffer
-    let pieces = T.lines text
-    modifyIORef' lineBuffer (<> pieces)
-    replicateM_ (length pieces) $ putMVar sema ()
-  return $ ChildProcess childProcess lineBuffer sema
+    let lines = T.lines text
+    void $ atomically $ traverse (\line -> writeTChan <$> lineQueue <*> pure line) lines
+  return $ ChildProcess childProcess lineQueue
 
 readLine :: ChildProcess -> IO Text
-readLine (ChildProcess childProcess lineBuffer sema) = do
-  takeMVar sema
-  atomicModifyIORef' lineBuffer (\(l:ls) -> (ls, l))
+readLine (ChildProcess childProcess lineQueue) = do
+  atomically (readTChan =<< lineQueue)
 
 writeLine :: ChildProcess -> Text -> IO ()
-writeLine (ChildProcess childProcess _ _) text = do
+writeLine (ChildProcess childProcess _) text = do
   inStream <- CP.stdin childProcess
   CP.write inStream (toJSString $ T.snoc text '\n')
