45cdeb0afc7eb9acf7bfc24072ff49d1321ab275
Callum Rogers <callumrogers@hotmail.co.uk>
2015-07-18 12:23:43 +0100

    Use MVar rather than timeout for test

--- haskell/test/ChildProcessSpec.hs
+++ haskell/test/ChildProcessSpec.hs
@@ -1,21 +1,34 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, TypeSynonymInstances, FlexibleInstances #-}
 
 module ChildProcessSpec where
 
 import Control.Monad
+import Control.Concurrent.MVar
 
 import Control.Concurrent
 import Test.Hspec
+import GHCJS.Types
 import GHCJS.Foreign
 
 import Node.ChildProcess
+
+instance Show JSString where
+  show = fromJSString
+
+foreign import javascript unsafe
+  "$1 === $2" js_str_eq :: JSString -> JSString -> JSBool
+
+instance Eq JSString where
+  str1 == str2 = fromJSBool (str1 `js_str_eq` str2)
 
 spec = do
   describe "Node.ChildProcess" $ do
     it "should spawn a process correctly" $ do
       childProcess <- spawn "echo" ["lel"] "."
       stream <- stdout childProcess
+      sema <- newEmptyMVar
       on stream "data" $ \buffer -> do
         str <- toString buffer
-        putStrLn (fromJSString str)
-      threadDelay $ 1000 * 1000
+        putMVar sema str
+      value <- takeMVar sema
+      value `shouldBe` "lel\n"
