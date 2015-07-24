0731f9c268273da49bc3a6146cc19cfa87483f24
Callum Rogers <callumrogers@hotmail.co.uk>
2015-07-25 00:45:52 +0100

    Better caching

--- circleci-docker-cache.sh
+++ circleci-docker-cache.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-CACHE_DIR="$HOME/cache"
+CACHE_DIR="$HOME/docker"
 IMAGE="$CACHE_DIR/image.tar"
-CABAL_SANDBOX="$CACHE_DIR/cabal-sandbox"
-DOCKER_STACK="$CACHE_DIR/docker-stack"
 DOCKER_TAG=crogers/stack-ide-atom-docker:stack-0.1.2.0_nightly-2015-06-28
 
-GIT_DIR="$HOME/stack-ide-atom"
-
 mkdir -p $CACHE_DIR
-
-ln -s "$GIT_DIR/haskell/.cabal-sandbox" $CABAL_SANDBOX
-ln -s "$GIT_DIR/.docker-stack" $DOCKER_STACK
 
 if [[ -e $IMAGE ]]; then
 	echo Loading docker image...
