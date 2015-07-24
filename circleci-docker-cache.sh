06a8eba0a0312d07c3d4047546794f0b75940118
Callum Rogers <callumrogers@hotmail.co.uk>
2015-07-25 00:37:43 +0100

    Also cache cabal sandbox and stack snapshots

--- circleci-docker-cache.sh
+++ circleci-docker-cache.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-CACHE_DIR="$HOME/docker"
+CACHE_DIR="$HOME/cache"
 IMAGE="$CACHE_DIR/image.tar"
+CABAL_SANDBOX="$CACHE_DIR/cabal-sandbox"
+DOCKER_STACK="$CACHE_DIR/docker-stack"
 DOCKER_TAG=crogers/stack-ide-atom-docker:stack-0.1.2.0_nightly-2015-06-28
 
+GIT_DIR="$HOME/stack-ide-atom"
+
 mkdir -p $CACHE_DIR
+
+ln -s "$GIT_DIR/haskell/.cabal-sandbox" $CABAL_SANDBOX
+ln -s "$GIT_DIR/.docker-stack" $DOCKER_STACK
+
 if [[ -e $IMAGE ]]; then
 	echo Loading docker image...
 	time docker load -i $IMAGE
